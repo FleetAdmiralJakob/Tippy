0caac1873cc25415fce2ae243d017d03b1f8301e
FleetAdmiralJakob <jakob.roessner@outlook.de>
2022-10-30 18:37:02 +0100

    Data Update
    #1 Added some countries
    #2 Added some currencies

--- app/src/main/java/com/jakob/tippy/MainActivity.kt
+++ app/src/main/java/com/jakob/tippy/MainActivity.kt
@@ -119,6 +119,9 @@ class MainActivity : AppCompatActivity() {
             1 -> 0
             2 -> 1
             3 -> 2
+            4 -> 3
+            5 -> 4
+            6 -> 5
             else -> 0
         }
         currenciesSpinner.setSelection(currency)
